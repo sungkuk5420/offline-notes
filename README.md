fee09423c29a36bac42ad7949fdd952adb9821a5
sungkuk5420 <sungkuk5420@gmail.com>
2020-03-01 11:54:13 +0900

    프로젝트 기본 셋팅 + 아폴로 클라이언트 오프라인 셋팅

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # offline-notes
 offline note with graphql + apollo + react 
+
+
+###  setting
+
+```
+yarn add apollo-cache-inmemory apollo-client apollo-link graphql react-apollo styled-components styled-reset apollo-link-state react-apollo react-router-dom react-markdown-renderer react-textarea-autosize graphql-tag
+```
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ApolloProvider} from 'react-apollo';
+import client from './apollo';
+import "./globalStyles";
+import App from './App';
+
+ReactDOM.render(<ApolloProvider client={client}>
+    <App/>
+</ApolloProvider>, document.getElementById('root'));
